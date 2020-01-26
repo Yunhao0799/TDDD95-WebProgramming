772f817a2659965272d1faa83fc9a6d35053c430
Yunhao0799 <yunhi.0799@gmail.com>
2020-01-26 18:54:42 +0100

    Finished_step_2
    Step 2 working

--- client.js
+++ client.js
@@ -1,8 +1,11 @@
-displayView = function(){
+displayView = function(viewId){
   // the code required to display a view
+  const viewContent = window.document.getElementById(viewId).innerText
+  window.document.getElementById('content').innerHTML = viewContent
 };
 window.onload = function(){
   //code that is executed as the page is loaded.
   //You shall put your own custom code here.
-  window.alert("Hello TDDD97!");
+  //window.alert("Hello TDDD97!");
+  displayView('welcomeview')
 };
