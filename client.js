a655aaf33ff04609840503f035bdf3e5fe9eee51
Mag39 <marie.miny@yahoo.fr>
2020-01-27 14:02:18 +0100

    Step3_done

--- client.js
+++ client.js
@@ -8,9 +8,10 @@ checkPassword = function(form) {
 
   var goodLength = function(password) {
     if(password.value.length < 5) {
-      var box1 = document.getElementById("password1");
-      var errorLength = document.createElement('p');
-      errorLength.textContent = "Password too short. You need at least 5 characters."; //put it in red and smaller
+      var box1 = document.getElementById("password1"); // !! need to erase the previous errors before
+      var errorLength = document.createElement('h5');
+      errorLength.setAttribute("id", "errorLength");
+      errorLength.textContent = "Password too short. You need at least 5 characters.";
       box1.insertAdjacentElement('afterend', errorLength);
       return false;
     } else {
@@ -21,8 +22,9 @@ checkPassword = function(form) {
   var samePwd = function(password1, password2) {
     if(password1.value != password2.value) {
       var box2 = document.getElementById("password2");
-      var errorSame = document.createElement('p');
-      errorSame.textContent = "It is not the same password"; //put it in red and smaller
+      var errorSame = document.createElement('h5');
+      errorSame.setAttribute("id", "errorSame");
+      errorSame.textContent = "It is not the same password";
       box2.insertAdjacentElement('afterend', errorSame);
       return false;
     } else {
@@ -30,8 +32,7 @@ checkPassword = function(form) {
     }
   }
 
-  if(goodLength(form.password1) && goodLength(form.password2) && samePwd(form.password1, form.password2)) {
-    //I'm not sure we need to check the length of the two passwords...
+  if(goodLength(form.password1) && samePwd(form.password1, form.password2)) {
     return true
   } else {
     return false;
