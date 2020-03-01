1b2c43a767cff829d6bb19cd0d3c681de4c319fb
Yunhao0799 <yunhi.0799@gmail.com>
2020-03-01 16:26:01 +0100

    Req_1_done

---
+++ Twidder_yunhao/database_helper.py
@@ -0,0 +1,172 @@
+################################################################################
+#                       database_helper.py                                     #
+# This file will contain all the functions that access and control the         #
+# database and shall contain some SQL scripts. This file will be used          #
+# by the server to access the database. This file shall NOT contain any        #
+# domain functions like signin or signup and shall only contain data-centric   #
+# functionality like find_user(), remove_user(), create_post() and … .         #
+# E.g. Implementing sign_in() in server.py shall involve a call to             #
+# find_user() implemented in database_helper.py .                              #
+################################################################################
+
+
+import sqlite3
+from flask import Flask, g, request
+
+
+DATABASE_URI = "database.db"
+
+def get_db():
+    db = getattr(g,'db', None)
+    if db is None:
+        db = g.db = sqlite3.connect(DATABASE_URI)
+
+    return db
+
+
+def disconnect_db():
+    db = getattr(g,'db', None)
+    if db is not None:
+        g.db.close()
+        g.db = None
+
+def check_user_password(email, password):
+    cursor = get_db().execute('select password from users where email like ?', [email])
+    rows = cursor.fetchall()
+    cursor.close()
+    result = []
+    for index in range(len(rows)):
+        result.append(rows[index][0])
+    if result==[]:
+        return False
+    if result[0] == password :
+        return True
+    else:
+        return False
+
+
+def link_token_to_user(email, token):
+    try:
+        get_db().execute("insert or replace into loggedUser values(?,?)", [email, token])
+        get_db().commit()
+        return True
+    except:
+        return False
+
+def save_new_user(email, password, firstname, familyname, gender, city, country, salt):
+    try:
+        get_db().execute("insert into users values(?,?,?,?,?,?,?,?)", [email, password, firstname, familyname, gender, city, country, salt])
+        get_db().commit()
+        return True
+    except:
+        return False
+
+
+def sign_out(token):
+    try:
+        get_db().execute("delete from loggedUser where token like ?", [token])
+        get_db().commit()
+        return True
+    except:
+        return False
+
+def get_user_data_by_token(token):
+    cursor = get_db().execute('select * from users where email like (select email from loggedUser where token like ?)', [token])
+    rows = cursor.fetchall()
+    cursor.close()
+    if rows==[]:
+        return None
+    else :
+        data = {"success" : True, "email" : rows[0][0], "firstname" : rows[0][2], "familyname" : rows[0][3], "gender" : rows[0][4], "city" : rows[0][5], "country" : rows[0][6]}
+        return data
+
+def get_user_data_by_email(email):
+    cursor = get_db().execute('select * from users where email like ?', [email])
+    rows = cursor.fetchall()
+    cursor.close()
+    if rows==[]:
+        return None
+    else:
+        index = 0
+        data = {"success" : True, "email" : rows[index][0], "firstname" : rows[index][2], "familyname" : rows[index][3], "gender" : rows[index][4], "city" : rows[index][5], "country" : rows[index][6]}
+        return data
+
+def get_user_messages_by_token(token):
+    cursor = get_db().execute('select sender, message from message where receiver like (select email from loggedUser where token like ?)', [token])
+    rows = cursor.fetchall()
+    cursor.close()
+    if rows==[]:
+        return None
+    else:
+        data = []
+        for index in range(len(rows)):
+            data.append({'sender' : rows[index][0], 'message' : rows[index][1]})
+        return data
+
+
+def get_user_messages_by_email(email):
+    cursor = get_db().execute('select sender, message from message where receiver like ?', [email])
+    rows = cursor.fetchall()
+    cursor.close()
+    if rows==[]:
+        return None
+    else:
+        data = []
+        for index in range(len(rows)):
+            data.append({'sender' : rows[index][0], 'message' : rows[index][1]})
+        return data
+
+
+def get_email_by_token(token):
+        cursor = get_db().execute('select email from users where email like (select email from loggedUser where token like ?)', [token])
+        rows = cursor.fetchall()
+        cursor.close()
+        if rows==[]:
+            return None
+        else:
+            data = []
+            for index in range(len(rows)):
+                data.append(rows[index][0])
+            return data
+
+def post_message(sender_mail, message, dest_email):
+        try:
+            get_db().execute("insert into message (sender, receiver, message) values(?,?,?)", [sender_mail, dest_email, message])
+            get_db().commit()
+            return True
+        except:
+            return False
+
+
+def check_if_email_exists(email):
+    cursor = get_db().execute('select email from users where email like ?', [email])
+    rows = cursor.fetchall()
+    cursor.close()
+    data = []
+    for index in range(len(rows)):
+        data.append(rows[index][0])
+    if data==[]:
+        return False
+    else:
+        return True
+
+
+def change_password(email, new_password, salt):
+    try:
+        get_db().execute("update users set password=?,salt=? where email=?;", [new_password, salt, email])
+        get_db().commit()
+        return True
+    except:
+        return False
+
+
+def get_users_salt(email):
+    cursor = get_db().execute('select * from users where email like ?', [email])
+    rows = cursor.fetchall()
+    cursor.close()
+    if rows==[]:
+        return None
+    else:
+        index = 0
+        data = {"password" : rows[index][1], "salt" : rows[index][7]}
+        return data
---
+++ Twidder_yunhao/static/client.js
@@ -0,0 +1,427 @@
+
+
+displayView = function(viewId){
+  // the code required to display a view
+  const viewContent = window.document.getElementById(viewId).innerText
+  window.document.getElementById('content').innerHTML = viewContent
+};
+
+window.onload = function(){
+  //code that is executed as the page is loaded.
+  if(this.localStorage.getItem("token") === null) {
+    displayView('welcomeview');
+  } else {
+    var token = this.localStorage.getItem("token");
+    checkSocket(token);
+    displayView("profileview");
+    document.getElementById("variousMess").style.display = "none";
+    infoPerso(token);
+    displayOwnMessages(token);
+  }
+};
+
+
+checkSignup = function(form) {
+  var goodLength = function(password) {
+    if(password.value.length < 5) {
+      document.getElementById("errorSignup").style.display = "none";
+      document.getElementById("okSignup").style.display = "none";
+      document.getElementById("wrongSame1").style.display = "none";
+      document.getElementById("wrongLength1").innerHTML = "";
+      document.getElementById("wrongLength1").style.display = "block";
+      var errorLength = document.createElement('h5');
+      errorLength.innerHTML = "Password too short. You need at least 5 characters.";
+      document.getElementById("wrongLength1").appendChild(errorLength);
+      return false;
+    } else {
+      return true;
+    }
+  };
+  var samePwd = function(password1, password2) {
+    if(password1.value != password2.value) {
+      document.getElementById("errorSignup").style.display = "none";
+      document.getElementById("okSignup").style.display = "none";
+      document.getElementById("wrongLength1").style.display = "none";
+      document.getElementById("wrongSame1").innerHTML = "";
+      document.getElementById("wrongSame1").style.display = "block";
+      var errorSame = document.createElement('h5');
+      errorSame.innerHTML = "It is not the same password";
+      document.getElementById("wrongSame1").appendChild(errorSame);
+      return false;
+    } else {
+      return true;
+    }
+  };
+  if(goodLength(form.password1) && samePwd(form.password1, form.password2)) {
+    var data = {"email" : form.email.value, "password" : form.password1.value, "firstname" : form.firstname.value, "familyname" : form.familyname.value, "gender" : form.gender.value, "city" : form.city.value, "country" : form.country.value};
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("POST", '/sign_up', true);
+    xhttp.onreadystatechange = function() {
+      if(this.readyState == 4 && this.status == 200) {
+        var signUpResponse = JSON.parse(this.responseText);
+        //console.log(signUpResponse);
+        if(signUpResponse.success==false) {
+          document.getElementById("errorSignup").style.display = "block";
+          document.getElementById("okSignup").style.display = "none";
+          document.getElementById("wrongLength1").style.display = "none";
+          document.getElementById("wrongSame1").style.display = "none";
+          var error = document.createElement('h5');
+          error.innerHTML = signUpResponse.message;
+          document.getElementById("errorSignup").appendChild(error);
+        } else {
+          document.getElementById("wrongSame1").style.display = "none";
+          document.getElementById("errorSignup").style.display = "none";
+          document.getElementById("wrongLength1").style.display = "none";
+          document.getElementById("okSignup").style.display = "block";
+          var ok = document.createElement('h4');
+          ok.innerHTML = signUpResponse.message;
+          document.getElementById("okSignup").appendChild(ok);
+          document.forms['sign'].reset();
+        }
+      }
+    }
+    xhttp.setRequestHeader("content-type", "application/json; charset=utf-8");
+    xhttp.send(JSON.stringify(data));
+  } else {
+    return false;
+  };
+};
+
+
+checkSignIn = function(form){
+
+  var user = form.logMail.value;
+  var password = form.logPswd.value;
+  var data = {"email" : user, "password" : password};
+  var xhttp = new XMLHttpRequest();
+  xhttp.open("PUT", '/sign_in', true);
+  xhttp.onreadystatechange = function() {
+    if(this.readyState == 4 && this.status == 200) {
+    var signInResponse = JSON.parse(this.responseText);
+    if (signInResponse.success == true) {
+      //console.log(signInResponse);
+      localStorage.setItem("token", signInResponse.message);
+      return window.onload();
+    } else {
+      document.getElementById('errorSignin').innerHTML = " ";
+      document.getElementById("errorSignin").style.display = "block";
+      var error = document.createElement('h5');
+      error.innerHTML = signInResponse.message;
+      document.getElementById("errorSignin").appendChild(error);
+      return false;
+    }
+  }
+  };
+  xhttp.setRequestHeader("content-type", "application/json; charset=utf-8");
+  xhttp.send(JSON.stringify(data));
+};
+
+function openTab(evt, tabName, element){
+  var i;
+  var tabs = document.getElementsByClassName("tab");
+  for(i = 0; i < tabs.length; i++) {
+    tabs[i].style.display = "none";
+  }
+  var tabsMenu = document.getElementsByClassName("tabsMenu");
+  for(i = 0; i < tabsMenu.length; i++) {
+     tabsMenu[i].style.backgroundColor = "";
+     tabsMenu[i].className = tabsMenu[i].className.replace(" highlighted", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " highlighted";
+};
+//The function hides all elements with the class name "tab" (display="none"), and displays the element with the given tab name (display="block");
+
+
+var changePswd = function(form){
+
+  var oldPswd = form.oldPswd.value;
+  var newPswd = form.newPswd.value;
+  var newPswd2 = form.newPswd2.value;
+  var token = this.localStorage.getItem("token");
+
+  var samePwd = function(password1, password2) {
+    if(password1 != password2) {
+      document.getElementById("errorNewPass").style.display = "none";
+      document.getElementById("okChange").style.display = "none";
+      document.getElementById("wrongLength").style.display = "none";
+      document.getElementById("wrongSame").innerHTML = "";
+      document.getElementById("wrongSame").style.display = "block";
+      var errorSame = document.createElement('h5');
+      errorSame.innerHTML = "It is not the same password";
+      document.getElementById("wrongSame").appendChild(errorSame);
+      return false;
+    } else {
+      return true;
+    }
+  }
+  var goodLength = function(password) {
+    if(password.length < 5) {
+      document.getElementById("errorNewPass").style.display = "none";
+      document.getElementById("okChange").style.display = "none";
+      document.getElementById("wrongSame").style.display = "none";
+      document.getElementById("wrongLength").innerHTML = "";
+      document.getElementById("wrongLength").style.display = "block";
+      var errorLength = document.createElement('h5');
+      errorLength.innerHTML = "Password too short. You need at least 5 characters.";
+      document.getElementById("wrongLength").appendChild(errorLength);
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  if(samePwd(newPswd, newPswd2) && goodLength(newPswd)) {
+    var xhttp = new XMLHttpRequest();
+    var data = {"token" : token, "old_password" : oldPswd, "new_password" : newPswd};
+    xhttp.open("POST", '/change_password', true);
+    xhttp.onreadystatechange = function() {
+      if(this.readyState == 4 && this.status == 200) {
+        var changePswdResponse = JSON.parse(this.responseText);
+        //console.log(changePswdResponse);
+        if(changePswdResponse.success==false) {
+          document.getElementById("errorNewPass").style.display = "block";
+          document.getElementById("okChange").style.display = "none";
+          document.getElementById("wrongLength").style.display = "none";
+          document.getElementById("wrongSame").style.display = "none";
+          var error = document.createElement('h5');
+          error.innerHTML = changePswdResponse.message;
+          document.getElementById("errorNewPass").appendChild(error);
+        } else {
+          document.getElementById("wrongSame").style.display = "none";
+          document.getElementById("errorNewPass").style.display = "none";
+          document.getElementById("wrongLength").style.display = "none";
+          document.getElementById("okChange").style.display = "block";
+          document.forms['changePassword'].reset();
+        }
+      }
+    }
+    xhttp.setRequestHeader("content-type", "application/json; charset=utf-8");
+    xhttp.send(JSON.stringify(data));
+  } else {
+    return false;
+  };
+};
+
+
+var logOut = function() {
+  var token = this.localStorage.getItem("token");
+  var data = {"token" : token};
+  var xhttp = new XMLHttpRequest();
+  xhttp.open("POST", '/sign_out', true);
+  xhttp.onreadystatechange = function() {
+    if(this.readyState == 4 && this.status == 200) {
+    var signOutResponse = JSON.parse(this.responseText);
+    if (signOutResponse.success==true) {
+      localStorage.removeItem('token');
+      location.reload();
+      return window.onload();
+    } else {
+      document.getElementById("variousMess").innerHTML = " ";
+      document.getElementById("variousMess").style.display = "block";
+      return false;
+    }
+  }
+  };
+  xhttp.setRequestHeader("content-type", "application/json; charset=utf-8");
+  xhttp.send(JSON.stringify(data));
+};
+
+
+var infoPerso = function(token){
+  var data = {"token" : token};
+  var xhttp = new XMLHttpRequest();
+  xhttp.open("POST", '/get/data/by_token', true);
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var infoResponse = JSON.parse(this.responseText);
+      //console.log(infoResponse);
+      if(infoResponse.success==true) {
+        document.getElementById("personalInfo").innerHTML = " ";
+        for(i in infoResponse) {
+          if(i!="success") {
+            var al = infoResponse[i];
+            var aux = document.createElement("li");
+            aux.innerHTML = i + ":  " + al;
+            document.getElementById("personalInfo").appendChild(aux);
+          }
+        }
+      } else {
+        var infoResponse = JSON.parse(this.responseText);
+        document.getElementById("personalInfo").innerHTML = " ";
+        var error = document.createElement('h5');
+        error.innerHTML = infoResponse.message;
+        document.getElementById("personalInfo").appendChild(error);
+      }
+    }
+   };
+  xhttp.setRequestHeader("content-type", "application/json; charset=utf-8");
+  xhttp.send(JSON.stringify(data));
+};
+
+var postOwnMessage = function(form) {
+  var token = this.localStorage.getItem("token");
+  var dest = null;
+  var message = form.message.value;
+  var data = {'token' : token, 'message' : message, 'email' : dest};
+  var xhttp = new XMLHttpRequest();
+  xhttp.open("POST", '/post_message', true);
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var postMessageResponse = JSON.parse(this.responseText);
+      if(postMessageResponse.success==false) {
+        document.getElementById("errorPost").style.display = "block";
+        var error = document.createElement('h5');
+        error.innerHTML = postMessageResponse.message;
+        document.getElementById("errorPost").appendChild(error);
+      } else {
+        displayOwnMessages(token);
+        document.forms['postMessOwn'].reset();
+      }
+    }
+   };
+  xhttp.setRequestHeader("content-type", "application/json; charset=utf-8");
+  xhttp.send(JSON.stringify(data));
+};
+
+var postmessageUser = function(form) {
+  var token = this.localStorage.getItem("token");
+  var dest = searchUser(document.forms["searchuser"]).dest;
+  var message = form.message.value;
+  var data = {'token' : token, 'message' : message, 'email' : dest};
+  var xhttp = new XMLHttpRequest();
+  xhttp.open("POST", '/post_message', true);
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var postMessageResponse = JSON.parse(this.responseText);
+      //console.log(postMessageResponse);
+      if(postMessageResponse.success==false) {
+        document.getElementById("errorPost2").style.display = "block";
+        var error = document.createElement('h5');
+        error.innerHTML = postMessageResponse.message;
+        document.getElementById("errorPost2").appendChild(error);
+      } else {
+        searchUser(document.forms["searchuser"]);
+        document.forms['postMessUser'].reset();
+      }
+    }
+   };
+  xhttp.setRequestHeader("content-type", "application/json; charset=utf-8");
+  xhttp.send(JSON.stringify(data));
+};
+
+
+var displayOwnMessages = function(token) {
+  var data = {"token" : token};
+  var xhttp = new XMLHttpRequest();
+  xhttp.open("POST", 'get/messages/by_token', true);
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var listMessage = JSON.parse(this.responseText);
+      //console.log(listMessage);
+      document.getElementById('wallMessage').innerHTML = " ";
+      var i;
+      for(i=0; i < listMessage.length; i++) {
+        var writer = listMessage[i].sender;
+        var content = listMessage[i].message;
+        var aux = document.createElement("li");
+        aux.innerHTML = writer + ":  " + content;
+        document.getElementById("wallMessage").appendChild(aux);
+      }
+    }
+  }
+  xhttp.setRequestHeader("content-type", "application/json; charset=utf-8");
+  xhttp.send(JSON.stringify(data));
+};
+
+var searchUser = function(form) {
+  var went_well=false;
+  var email = form.user.value;
+  var token = this.localStorage.getItem("token");
+  var data = {"token" : token, "email" : email};
+  var xhttp1 = new XMLHttpRequest();
+  xhttp1.open("POST", '/get/data/by_email', true);
+  xhttp1.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+       var userDataResponse = JSON.parse(this.responseText);
+       //console.log(userDataResponse);
+       if(userDataResponse.success==false) {
+          went_well=false;
+          document.getElementById('errorSearch').innerHTML = " ";
+          document.getElementById("resultSearch").style.display="none";
+          document.getElementById("errorSearch").style.display="block";
+          var post = document.createElement('h5');
+          post.innerHTML = userDataResponse.message;
+          document.getElementById("errorSearch").appendChild(post);
+          return false;
+      } else {
+        went_well=true;
+        document.getElementById("resultSearch").style.display="block";
+        document.getElementById("errorSearch").style.display="none";
+        document.getElementById('userInfo').innerHTML = " ";
+        document.getElementById('wallMessageUser').innerHTML = " ";
+        for(i in userDataResponse) {
+          if(i!="success") {
+            var al = userDataResponse[i];
+            var aux = document.createElement("li");
+            aux.innerHTML = i + ":  " + al;
+            document.getElementById("userInfo").appendChild(aux);
+          }
+        }
+        var xhttp2 = new XMLHttpRequest();
+        xhttp2.open("POST", '/get/messages/by_email', true);
+        xhttp2.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+              var userMessagesResponse = JSON.parse(this.responseText);
+              //console.log(userMessagesResponse);
+              for(i in userMessagesResponse) {
+                var writer = userMessagesResponse[i].sender;
+                var content = userMessagesResponse[i].message;
+                var aux = document.createElement("li");
+                aux.innerHTML = writer + ":  " + content;
+                document.getElementById("wallMessageUser").appendChild(aux);
+              }
+             }
+            }
+          xhttp2.setRequestHeader("content-type", "application/json; charset=utf-8");
+          xhttp2.send(JSON.stringify(data));
+      }
+    }
+  };
+  xhttp1.setRequestHeader("content-type", "application/json; charset=utf-8");
+  xhttp1.send(JSON.stringify(data));
+  return {
+    dest:email,
+    result:false,
+  }
+};
+
+var refreshOwnWall = function() {
+  var token = this.localStorage.getItem("token");
+  displayOwnMessages(token);
+};
+
+var refreshUserWall = function() {
+  searchUser(document.forms["searchuser"]);
+};
+
+
+function checkSocket(token){
+  var socket = new WebSocket("ws://localhost:5000/api");
+  // socket.onerror = function(error){
+  //   console.log("WS Error: " + error);
+  // }
+  socket.onmessage = function(event){
+    if(event.data == "sign_out"){
+      console.log("in the if event loop");
+      localStorage.removeItem("token");
+      socket.close();
+      // displayView('welcomeview');
+      window.onload();
+    }
+  }
+  socket.onopen = function(){
+    console.log("in onopen function");
+    socket.send(token);
+  }
+};
